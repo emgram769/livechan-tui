9628b95435430e391b16d700d60e1bcde5bcffc1
Bram <bwasti@gmail.com>
2014-05-16 17:58:40 -0400

    working but no interface

--- livechan-client.py
+++ livechan-client.py
@@ -3,7 +3,10 @@ import urllib2
 import cookielib
 import os
 import json
-from websocket import create_connection
+from socketIO_client import SocketIO
+import curses
+import thread
+import time
 
 cookies = cookielib.LWPCookieJar()
 handlers = [
@@ -59,31 +62,57 @@ def login():
     livechan_pass = get_password()
     if livechan_pass == "":
         login()
+    return livechan_pass
 
 def get_data(chat):
     data_response = fetch('https://livechan.net/data/'+chat)
     json_data = json.loads(data_response.read())
-    for i in json_data:
+    for i in json_data[::-1]:
         print i[u"name"]
         print i[u"body"]
         print
 
 def main_chat(chat):
-    chat_body = raw_input()
+    chat_body = raw_input("> ")
     mainresp = post_chat(chat_body, chat)
     print mainresp.read()
 
+def on_chat(*args):
+    print 'chat', args
+
+def on_user_count(*args):
+    print 'chat', args
+
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
 
 #login
-login()
+cookie = login()
 
 chat_room = raw_input("choose room: ")
 print
 get_data(chat_room)
-ws = create_connection("wss://livechan.net/")
-result =  ws.recv()
-print "Received '%s'" % result
-ws.close()
+
+socketIO = SocketIO('https://livechan.net',
+        cookies={'password_livechan': cookie})
+
+socketIO.on('chat', on_chat)
+socketIO.on('user_count', on_user_count)
+socketIO.emit('subscribe', chat_room);
+
+thread.start_new_thread ( socketIO.wait, () )
+
+#stdscr = curses.initscr()
+#curses.noecho()
+#curses.cbreak()
+#stdscr.keypad(1)
+
+while(True):
+    main_chat(chat_room)
+    time.sleep(7)
+
 #main_chat(chat_room)
 
 print "done"
+
+
